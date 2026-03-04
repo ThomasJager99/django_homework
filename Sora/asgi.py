fe77172fba7b39ae613f504188813398ee66afdb
ThomasJager99 <artemzubchenko1@gmail.com>
2026-03-04 19:06:04 +0100

    nothing changed on this stage

---
+++ Sora/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Sora project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sora.settings')
+
+application = get_asgi_application()
